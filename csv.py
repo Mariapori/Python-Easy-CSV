8a10b700aab7e7ece1f82d053b454d4f40442b02
Topias Mariapori <Mariapori@users.noreply.github.com>
2018-12-21 20:15:02 +0200

    Add files via upload

---
+++ csv.py
@@ -0,0 +1,19 @@
+import os
+
+def Tallenna(tiedostonimi):
+    f = open(tiedostonimi, "w")
+    f.write("Nimi;Ikä;Ammatti;\n")
+    f.write(Nimi + ";" + Ikä + ";" + Ammatti + ";")
+    f.close()
+    print("Tallennettu onnistuneesti!")
+
+
+Nimi = input("Nimi: ")
+Ikä = input("Ikä: ")
+Ammatti = input("Ammatti: ")
+tallennetaanko = input("Tallennetaan? y/n: ")
+if tallennetaanko == "y":
+    tiedostonimi = input("Tiedostonimi: ") + ".csv"
+    Tallenna(tiedostonimi)
+else:
+    print("Selvä ja näkemiin!")
